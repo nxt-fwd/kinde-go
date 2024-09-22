418ac1d9c444473f2e5b251f65dc824d5da98588
Hans Song <hans.m.song@gmail.com>
2024-09-22 16:09:58 +1000

    separate api application type

--- op_apis.go
+++ op_apis.go
@@ -8,11 +8,18 @@ import (
 
 // https://kinde.com/api/docs/#kinde-management-api-apis
 type API struct {
-	ID              string        `json:"id"`
-	Name            string        `json:"name"`
-	Audience        string        `json:"audience"`
-	IsManagementAPI bool          `json:"is_management_api"`
-	Applications    []Application `json:"applications,omitempty"`
+	ID              string           `json:"id"`
+	Name            string           `json:"name"`
+	Audience        string           `json:"audience"`
+	IsManagementAPI bool             `json:"is_management_api"`
+	Applications    []APIApplication `json:"applications,omitempty"`
+}
+
+type APIApplication struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Type     string `json:"type"`
+	IsActive *bool  `json:"is_active"`
 }
 
 type ListAPIResponse struct {
--- op_applications.go
+++ op_applications.go
@@ -1,4 +1,8 @@
 package kinde
 
 // https://kinde.com/api/docs/#kinde-management-api-applications
-type Application struct{}
+type Application struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
